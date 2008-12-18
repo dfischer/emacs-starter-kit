519c1d3708b2c09404d142d56961d2ec3a4409c8
DFischer <me@danielfischer.com>
2008-12-18 08:36:21 -0800

    Changed the background color of paren-face-match-light because of how awful it looked before... e.g: http://skitch.com/dfischer/67n8/users-armageddon-.emacs.d-elpa-to-submit-blackboard.el

---
+++ custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(paren-match-face (quote paren-face-match-light))
+ '(paren-sexp-mode t))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(paren-face-match-light ((((class color)) (:background "#960055")))))
