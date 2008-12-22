36e04beca780c70394f1f3f05b50ab0e78efdcf1
DFischer <me@danielfischer.com>
2008-12-22 05:32:41 -0800

    Fixed some interesting things

--- Armageddon.el
+++ Armageddon.el
@@ -9,3 +9,10 @@
 ;; (color-theme-charcoal-black)
 (load-file "~/.emacs.d/elpa-to-submit/blackboard.el")
 (color-theme-blackboard)
+
+;; Highlighting mode functionality - also related to deleting regions
+;; of text
+;; easier... http://www.emacswiki.org/cgi-bin/wiki?TransientMarkMode
+;; without it, you can't highlight text then hit 'del' to remove it.
+(transient-mark-mode 1)
+(delete-selection-mode 1)
